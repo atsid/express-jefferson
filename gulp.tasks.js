e2933a07a72aa6990a80935a29408e919cfd82cb
Chris Trevino <chris.trevino@atsid.com>
2015-03-26 12:48:16 -0700

    Removing Coveralls integration

--- gulp.tasks.js
+++ gulp.tasks.js
@@ -7,7 +7,6 @@ var gulp = require('gulp'),
     changed = require('gulp-changed'),
     runSequence = require('run-sequence'),
     istanbul = require('gulp-istanbul'),
-    coveralls = require('gulp-coveralls'),
     isparta = require('isparta'),
     del = require('del'),
     MOCHA_REPORTER = 'nyan',
@@ -57,10 +56,6 @@ gulp.task('static-analysis', [
     'lint',
     'jscs'
 ]);
-gulp.task('report-coverage', () => {
-    return gulp.src('coverage/**/lcov.info')
-        .pipe(coveralls());
-});
 
 
 /**
@@ -111,7 +106,6 @@ gulp.task('ci-build', (cb) => {
     runSequence(
         'ci-config',
         'build',
-        'report-coverage',
         cb
     );
 });
